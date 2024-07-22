f0b750223694f17932548108539141b7393d711b
ClintonCMOs <orengecmo@gmail.com>
2024-07-23 00:40:05 +0300

    updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,16 +3,13 @@ export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
 }
-
 // Function to return a specific string
 export function getLast() {
   return " is okay";
 }
-
 // Function to concatenate strings using let
 export function taskNext() {
   let combination = "But sometimes let";
   combination += getLast();
-
   return combination;
 }
