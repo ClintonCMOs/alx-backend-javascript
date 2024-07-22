c613caccd265c862f5015ea4d0b6187b3b220e0a
ClintonCMOs <orengecmo@gmail.com>
2024-07-23 00:30:08 +0300

    updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,21 @@
 export function taskFirst() {
+  // Declare a constant variable `task` and assign a string value to it.
   const task = "I prefer const when I can.";
+  // Return the value of `task`.
   return task;
 }
 
 export function getLast() {
+  // Return a string value.
   return " is okay";
 }
 
 export function taskNext() {
+  // Declare a variable `combination` using let and assign a string value to it.
   let combination = "But sometimes let";
+  // Concatenate the return value of `getLast` function to `combination`.
   combination += getLast();
 
+  // Return the concatenated string.
   return combination;
 }
