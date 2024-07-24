b2b2372bb817790db46c9d62aa07a5a31f8e5a24
ClintonCMOs <orengecmo@gmail.com>
2024-07-25 01:33:40 +0300

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("It worked!");
-      } else {
-        reject(new Error("It broke"));
-      }
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
